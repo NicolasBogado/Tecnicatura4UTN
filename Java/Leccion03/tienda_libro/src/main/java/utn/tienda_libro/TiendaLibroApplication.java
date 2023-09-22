9289f66e6e6042cb3ce4b33fd730a87075b4e1f5
Nicolas <nicolasbogadouai@gmail.com>
2023-09-21 23:15:47 -0300

    Leccion03 Sistema tienda de libros Parte 1
    Configuración del proyecto.

---
+++ Java/Leccion03/tienda_libro/src/main/java/utn/tienda_libro/TiendaLibroApplication.java
@@ -0,0 +1,13 @@
+package utn.tienda_libro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibroApplication.class, args);
+	}
+
+}
---
+++ Java/Leccion03/tienda_libro/src/test/java/utn/tienda_libro/TiendaLibroApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.tienda_libro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
