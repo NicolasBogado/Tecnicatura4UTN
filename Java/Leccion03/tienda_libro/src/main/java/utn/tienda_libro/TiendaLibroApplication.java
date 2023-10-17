57b8eaae4e961b10f121d70c39c5a58da89a2907
Nicolas <nicolasbogadouai@gmail.com>
2023-10-17 18:12:23 -0300

    clase 8
    java con Spring tienda libros

--- Java/Leccion03/tienda_libro/src/main/java/utn/tienda_libro/TiendaLibroApplication.java
+++ Java/Leccion03/tienda_libro/src/main/java/utn/tienda_libro/TiendaLibroApplication.java
@@ -1,13 +1,29 @@
 package utn.tienda_libro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libro.vista.LibroForm;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class TiendaLibroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibroApplication.class, args);
-	}
 
+		ConfigurableApplicationContext contextoSpring =
+				new SpringApplicationBuilder(TiendaLibroApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+		// ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() ->{ // método lambda
+		// obtenemos el objeto from a través del spring
+		LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+		libroForm.setVisible(true);
+		});
+	}
 }
--- Java/Leccion03/tienda_libro/src/main/java/utn/tienda_libro/vista/LibroForm.java
+++ Java/Leccion03/tienda_libro/src/main/java/utn/tienda_libro/vista/LibroForm.java
@@ -1,13 +1,20 @@
 package utn.tienda_libro.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libro.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
+@Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
+    
 
     @Autowired
     public LibroForm(LibroServicio libroServicio){
@@ -20,5 +27,17 @@ public class LibroForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900,700);
+        // para obtener la dimension de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x,y);
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
     }
 }
